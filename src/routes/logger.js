f8b1b87f6dcbd0f0b3b5b3c539a8e11ab7a03cd8
Jonathan Arriaza <jaarriaza.e@gmail.com>
2024-04-24 22:56:54 -0400

    Arreglo comit anterior

--- src/routes/logger.js
+++ src/routes/logger.js
@@ -15,7 +15,7 @@ const prodLogger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'errors.log', level: 'error' }),
         new winston.transports.File({ filename: 'combined.log' })
     ]
 })
